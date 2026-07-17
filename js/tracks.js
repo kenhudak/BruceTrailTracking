955e25f564f166ac048bbd24c90605da7936641f
Ken Hudak <kenhudak@gmail.com>
2026-07-17 15:28:14 -0400

    add more Blue Mountain track sections.

--- js/tracks.js
+++ js/tracks.js
@@ -23,6 +23,8 @@ const TRACK_FILES = [
   "20251004_BurlingtonDay1_20590461353.gpx",
   "20251005_BurlingtonDay2_20601435417.gpx",
   "20241024_BlueMnt.gpx",
+  "20241021_BlueMnt.gpx",
+  "20241022_BlueMnt.gpx",
   // "2026-07-19_cyprus-lake-to-crane-lake.gpx",
 ];
 
